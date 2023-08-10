299fa798f8aa3cc5fdb3b27c698b5ce00823b0e8
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-10 20:05:02 +0200

    Add index to scoresController

--- server/controllers/scoresController.js
+++ server/controllers/scoresController.js
@@ -7,8 +7,27 @@ const Map = require("../models/map");
 
 const asyncHandler = require("../asyncHandler");
 
+exports.index = asyncHandler(async (req, res, next) => {
+    const mapId = req.params.mapId;
+
+    const map = await Map.findById(mapId);
+
+    if (!map) return next(new Error(`Map ${mapId} not found`));
+
+    const scores = map.scores.sort((a, b) => a.seconds - b.seconds);
+
+    const data = {
+        message: "Scores found",
+        mapId,
+        scores,
+    };
+
+    debug(data);
+    res.json(data);
+});
+
 exports.create = [
-    body("playerName").optional({ checkFalsy: true }).isString().trim().escape(),
+    body("playerName").optional().isString().trim().escape(),
     body("seconds", "Seconds are required").isNumeric(),
     asyncHandler(async (req, res, next) => {
         const errors = validationResult(req);
@@ -20,7 +39,10 @@ exports.create = [
 
         const mapId = req.params.mapId;
 
-        const score = new Score(req.body);
+        const score = new Score({
+            playerName: req.body.playerName || undefined,
+            seconds: req.body.seconds,
+        });
 
         const map = await Map.findById(mapId);
 
